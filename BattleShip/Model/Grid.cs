daf523040f1e90805e0cb25f0f6354bd428a3816
andziev <ivan.andziev@students.finki.ukim.mk>
2017-05-03 16:32:21 +0200

    fixing

---
+++ BattleShip/Model/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleShip.Model
+{
+    class Grid : DataGridView
+    {
+        protected override void PaintBackground(Graphics graphics, Rectangle clipBounds, Rectangle gridBounds)
+        {
+            base.PaintBackground(graphics, clipBounds, gridBounds);
+            Rectangle rectSource = new Rectangle(this.Location.X, this.Location.Y, this.Width, this.Height);
+            Rectangle rectDest = new Rectangle(0, 0, rectSource.Width, rectSource.Height);
+
+            Bitmap b = new Bitmap(Parent.ClientRectangle.Width, Parent.ClientRectangle.Height);
+            Graphics.FromImage(b).DrawImage(this.Parent.BackgroundImage, Parent.ClientRectangle);
+
+
+            graphics.DrawImage(b, rectDest, rectSource, GraphicsUnit.Pixel);
+            SetCellsTransparent();
+        }
+
+
+        public void SetCellsTransparent()
+        {
+            EnableHeadersVisualStyles = false;
+            ColumnHeadersDefaultCellStyle.BackColor = Color.Transparent;
+            RowHeadersDefaultCellStyle.BackColor = Color.Transparent;
+
+
+            foreach (DataGridViewColumn col in Columns)
+            {
+                col.DefaultCellStyle.BackColor = Color.Transparent;
+                col.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+            }
+        }
+    }
+}
