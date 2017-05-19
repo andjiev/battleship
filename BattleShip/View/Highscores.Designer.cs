7683d0c5d8965519054b84f024c9d1546fd41a6b
DimovskiM <mihajlo.d96@hotmail.com>
2017-05-19 14:02:01 +0200

    Implemented some featured to Highscores.

--- BattleShip/View/Highscores.Designer.cs
+++ BattleShip/View/Highscores.Designer.cs
@@ -28,47 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox = new System.Windows.Forms.GroupBox();
-            this.lbHighscores = new System.Windows.Forms.ListBox();
-            this.groupBox.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox
-            // 
-            this.groupBox.Controls.Add(this.lbHighscores);
-            this.groupBox.Location = new System.Drawing.Point(12, 12);
-            this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(256, 240);
-            this.groupBox.TabIndex = 0;
-            this.groupBox.TabStop = false;
-            this.groupBox.Text = "Highscores";
-            // 
-            // lbHighscores
-            // 
-            this.lbHighscores.FormattingEnabled = true;
-            this.lbHighscores.Location = new System.Drawing.Point(6, 21);
-            this.lbHighscores.Name = "lbHighscores";
-            this.lbHighscores.Size = new System.Drawing.Size(238, 212);
-            this.lbHighscores.TabIndex = 0;
             // 
             // Highscores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.groupBox);
+            this.ClientSize = new System.Drawing.Size(301, 262);
             this.Name = "Highscores";
             this.Text = "Highscores";
             this.Load += new System.EventHandler(this.Highscores_Load);
             this.Shown += new System.EventHandler(this.Highscores_Shown);
-            this.groupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox;
-        private System.Windows.Forms.ListBox lbHighscores;
     }
 }
--- BattleShip/View/Highscores.cs
+++ BattleShip/View/Highscores.cs
@@ -11,16 +11,34 @@ using System.IO;
 using BattleShip.Model;
 namespace BattleShip.View
 {
+
     public partial class Highscores : Form
     {
+        List<Label> lblList;
+        Point p;
         public Highscores()
         {
+            p = new Point { X = 0, Y = 15 };
+            lblList = new List<Label>();
             InitializeComponent();
+            readFile();
+            
+          //  MessageBox.Show(lblList.Count.ToString());
+
         }
 
         private void Highscores_Load(object sender, EventArgs e)
         {
-           
+            foreach (Label l in lblList)
+            {
+            //    MessageBox.Show(l.ToString());
+                l.Location = p;
+                l.Font = new Font("Arial", 10);
+               
+                this.Controls.Add(l);
+                p = new Point { X = p.X, Y = p.Y+30 };
+                
+             }
 
         }
         public void readFile()
@@ -28,7 +46,8 @@ namespace BattleShip.View
             var fs = File.OpenRead(Application.StartupPath + "\\highscores.txt");
           //  MessageBox.Show("Reading!");
             var reader = new StreamReader(fs);
-            while (!reader.EndOfStream)
+            int I = 0;
+            while (I<5)
             {
                 var line = reader.ReadLine();
                 var values = line.Split(';');
@@ -36,8 +55,12 @@ namespace BattleShip.View
                 int.TryParse(values[1], out p);
                 //MessageBox.Show(values[0]);
                 Score s = new Score(values[0], p);
-                lbHighscores.Items.Add(s);
+                Label l = new Label();
+                l.Text = (I+1)+"."+s.ToString();
+                lblList.Add(l);
+                I++;
             }
+
         }
 
         private void label1_Click(object sender, EventArgs e)
