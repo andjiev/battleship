a1d407d7fb0ca1c79907596bb0c68ecd41d4ab96
Shampit <vlatko.zmejkoski@students.finki.ukim.mk>
2017-05-26 21:29:24 +0200

    Typos correction

--- BattleShip/Properties/Resources.Designer.cs
+++ BattleShip/Properties/Resources.Designer.cs
@@ -260,6 +260,16 @@ namespace BattleShip.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap howTO1 {
+            get {
+                object obj = ResourceManager.GetObject("howTO1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         public static System.Drawing.Icon Messbook_Outdated_Ship_Wheel {
